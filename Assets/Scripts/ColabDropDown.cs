00e3cb0dbe31fbfec38e9d8062186bef48c2c385
Nadine Petzka <nadine.petzka-hk@uni-wuppertal.de>
2023-08-25 13:28:23 +0200

    Menu issue fixed

--- Assets/Scripts/ColabDropDown.cs
+++ Assets/Scripts/ColabDropDown.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
-
+using UnityEngine.UI;
 
 public class ColabDropDown : MonoBehaviour
 {
     public GameObject ExitWindow;
-    public GameObject OptionPanel; 
+    public GameObject OptionPanel;
+    public TMP_Dropdown ColabDD;  
 
     public void DropDownChoice(int index)
     {
@@ -18,7 +19,7 @@ public class ColabDropDown : MonoBehaviour
             case 1: OptionenScene.GoToOptionsScene(); break;
             case 2: SceneManager.LoadScene(3); break; // hier noch current player daten speichern >> um beim zurück drücken genau dort wieder aufzutauchen
             case 3: ExitWindow.SetActive(true); break;
-            case 4: OptionPanel.SetActive(true); break;
+            case 4: OptionPanel.SetActive(true); ColabDD.value = 0;  break;
         }
     }
 }
